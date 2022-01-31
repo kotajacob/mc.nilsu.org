81af9ce11ba4d455ff5fc298bc9ca5e594ec5924
Dakota Walsh <kota@nilsu.org>
2022-02-01 11:40:35 +1300

    add option URL field to keySlices

--- keyslice.go
+++ keyslice.go
@@ -7,19 +7,21 @@ import (
 	"strings"
 )
 
-// keySlice is a slice of entries which contain key and value strings. They may
-// be parsed from a file with one entry per line with the format `key;value\n`.
+// keySlice is a slice of entries which contain key, value, and optional url
+// strings. They may be parsed from a file with one entry per line with the
+// format `key;value;url\n`.
 type keySlice []entry
 
-// entry holds key and value strings as part of a keySlice.
+// entry holds key value and url strings as part of a keySlice.
 type entry struct {
 	Key   string
 	Value string
+	URL   string
 }
 
-// parseKeyFile reads a file of keys and values in the format `key:value\n` and
-// stores it in a keySlice. If a line does not have 2 fields or otherwise cannot
-// be read an error is returned.
+// parseKeyFile reads a file of keys, values, and urls in the format
+// `key:value;url\n` and stores it in a keySlice. If a line has less than 2
+// fields or otherwise cannot be read an error is returned.
 func parseKeyFile(name string) (keySlice, error) {
 	file, err := os.Open(name)
 	if err != nil {
@@ -32,15 +34,19 @@ func parseKeyFile(name string) (keySlice, error) {
 
 	for scanner.Scan() {
 		t := strings.Split(scanner.Text(), ";")
-		if len(t) != 2 {
+		var e entry
+
+		if len(t) >= 2 {
+			e.Key = t[0]
+			e.Value = t[1]
+		} else {
 			return nil, fmt.Errorf("failed parsing %v: "+
 				"invalid number of fields: %v\n", name, scanner.Text())
 		}
-
-		e := entry{
-			Key:   t[0],
-			Value: t[1],
+		if len(t) == 3 {
+			e.URL = t[2]
 		}
+
 		entries = append(entries, e)
 	}
 	return entries, scanner.Err()
